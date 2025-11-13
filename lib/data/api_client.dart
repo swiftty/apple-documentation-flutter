4800f9c2a6cdacff786bea97b65e5e289fc11070
swiftty <62803132+swiftty@users.noreply.github.com>
2025-11-13 18:51:45 +0900

    Fix: Remove unnecessary flutter_riverpod import

--- lib/data/api_client.dart
+++ lib/data/api_client.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:http/http.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
